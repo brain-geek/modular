84fa6eec599f95cebaef118d93dcb011509ab755
Alex Rozumey <brain-geek@yandex.ua>
2012-07-24 11:15:27 +0300

    Remove obsolete duplicate of test rails app

--- modular-app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ModularApp::Application.config.secret_token = 'b1788b08fe0ecb92fa795521cb64c268482bdd967a1d064341e13eadef944bc96ac1552af9c104521b3f5743aac8107832ed689ba2404890275733697ffddc41'
--- modular-app/config/routes.rb
+++
@@ -1,7 +0,0 @@
-ModularApp::Application.routes.draw do
-  get "cached_for_time/index"
-
-  root :to => "example#index"
-  
-  match ':controller/(:action)'
-end
