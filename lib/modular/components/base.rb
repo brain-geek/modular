de8c26f87a4ecee532fc1ed7e494c72f5c488cea
Alex Rozumey <brain-geek@yandex.ua>
2012-07-25 16:45:26 +0300

    Add forgotten html safe

--- lib/modular/components/base.rb
+++ lib/modular/components/base.rb
@@ -30,7 +30,7 @@ module Modular
             path = Rails.root + "app/views/components/#{type}.mst"
             template = File.open(path, "rb").read
 
-            Mustache.render(template, @attributes)
+            Mustache.render(template, @attributes).html_safe
           end
         end
       end
